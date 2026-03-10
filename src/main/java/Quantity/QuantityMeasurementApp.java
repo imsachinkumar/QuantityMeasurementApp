5acd65a3f6ca0931c72e67eaaa9f013ca6af6838
Sachin Kumar <sachinthakurela2005@gmail.com>
2026-03-10 22:04:06 +0530

    [Sachin Kumar] add. Added UC11

--- src/main/java/Quantity/QuantityMeasurementApp.java
+++ src/main/java/Quantity/QuantityMeasurementApp.java
@@ -1,41 +1,30 @@
 package Quantity;
 
 import UtilityClasses.*;
-
 public class QuantityMeasurementApp {
 
     public static <U extends IMeasurable>
     void demonstrateEquality(Quantity<U> q1, Quantity<U> q2) {
 
-        System.out.println("Compare " + q1 +
-                " and " + q2 +
-                " -> Equal: " + q1.equals(q2));
+        System.out.println("Compare " + q1 + " and " + q2 + " -> Equal: " + q1.equals(q2));
     }
 
     public static <U extends IMeasurable>
     void demonstrateConversion(Quantity<U> quantity, U target) {
 
-        System.out.println("Convert " + quantity +
-                " to " + target +
-                " -> " + quantity.convertTo(target));
+        System.out.println("Convert " + quantity + " to " + target + " -> " + quantity.convertTo(target));
     }
 
     public static <U extends IMeasurable>
     void demonstrateAddition(Quantity<U> q1, Quantity<U> q2, U target) {
 
-        System.out.println("Add " + q1 +
-                " and " + q2 +
-                " -> " + q1.add(q2, target));
+        System.out.println("Add " + q1 + " and " + q2 + " -> " + q1.add(q2, target));
     }
 
     public static void main(String[] args) {
+        Quantity<LengthUnit> l1 = new Quantity<>(1.0, LengthUnit.FEET);
 
-        // Length Example
-        Quantity<LengthUnit> l1 =
-                new Quantity<>(1.0, LengthUnit.FEET);
-
-        Quantity<LengthUnit> l2 =
-                new Quantity<>(12.0, LengthUnit.INCH);
+        Quantity<LengthUnit> l2 = new Quantity<>(12.0, LengthUnit.INCH);
 
         demonstrateEquality(l1, l2);
         demonstrateConversion(l1, LengthUnit.INCH);
@@ -45,11 +34,17 @@ public class QuantityMeasurementApp {
         Quantity<WeightUnit> w1 =
                 new Quantity<>(1.0, WeightUnit.KILOGRAM);
 
-        Quantity<WeightUnit> w2 =
-                new Quantity<>(1000.0, WeightUnit.GRAM);
+        Quantity<WeightUnit> w2 = new Quantity<>(1000.0, WeightUnit.GRAM);
 
         demonstrateEquality(w1, w2);
         demonstrateConversion(w1, WeightUnit.GRAM);
         demonstrateAddition(w1, w2, WeightUnit.KILOGRAM);
+        Quantity<VolumeUnit> v1 =
+        new Quantity<>(1.0, VolumeUnit.LITRE);
+
+        Quantity<VolumeUnit> v2 =
+        new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
+
+       demonstrateEquality(v1, v2);
     }
 }
